4076d3a3a31705cadbdc0d436851cff56ef28c7f
Logan T <178672247+LD2343@users.noreply.github.com>
2026-01-12 21:44:08 -0600

    lab 1b

---
+++ Lab 1/Lab 1b/97-locals.tf
@@ -0,0 +1,6 @@
+############################################
+# Locals (naming convention: bos-*) (Brotherhood Of Steel)
+############################################
+locals {
+  name_prefix = var.project_name
+}
